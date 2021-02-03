68cfc7ffdce37e6974bf4786d7b39c99c2511d33
brittanymbui <brittymbui@gmail.com>
2021-02-02 16:11:57 -0800

    adds app configurations, all templates, views, routes, and Creature model

---
+++ main_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Creature
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def creatures_index(request):
+    creatures = Creature.objects.all()
+    return render(request, 'creatures/creatures_index.html', { 'creatures': creatures})
+
+def creatures_details(request, creature_id):
+    creature = Creature.objects.get(id=creature_id)
+    return render(request, 'creatures/creatures_details.html', { 'creature': creature })
