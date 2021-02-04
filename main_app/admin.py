94518a00891deb70b103ee176ded2a09b4160d84
brittanymbui <brittymbui@gmail.com>
2021-02-04 15:44:44 -0800

    image upload feature is functional

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Creature, Feeding
+from .models import Creature, Feeding, Photo
 # Register your models here.
 admin.site.register(Creature)
 admin.site.register(Feeding)
+admin.site.register(Photo)
